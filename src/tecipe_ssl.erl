fccb36a5f685983e11f703f33e6bab10d2afc5ec
Hamidreza Soleimani <hamidreza.s@gmail.com>
2016-11-08 17:21:09 +0330

    make transport modules generic

--- src/tecipe_ssl.erl
+++ src/tecipe_ssl.erl
@@ -1,7 +1,8 @@
 -module(tecipe_ssl).
 
--export([listen/2, accept/1, accept/2, setopts/2, send/2, recv/2,
-	 controlling_process/2, peername/1, sockname/1, getstat/1, close/1]).
+-export([listen/2, accept/1, accept/2, send/2, recv/2,
+	 controlling_process/2, peername/1, sockname/1,
+	 setopts/2, getopts/2, getstat/1, close/1]).
 
 -include("tecipe.hrl").
 
@@ -27,26 +28,47 @@ do_accept(Sock, Timeout) ->
             {error, Reason}
     end.
 
-setopts(#tecipe_socket{inet_socket = Sock}, Opts) ->
-    ssl:setopts(Sock, Opts).
-
 send(#tecipe_socket{inet_socket = Sock}, Data) ->
+    ssl:send(Sock, Data);
+send(Sock, Data) ->
     ssl:send(Sock, Data).
 
 recv(#tecipe_socket{inet_socket = Sock}, Length) ->
+    ssl:recv(Sock, Length);
+recv(Sock, Length) ->
     ssl:recv(Sock, Length).
 
 controlling_process(#tecipe_socket{inet_socket = Sock}, Pid) ->
+    ssl:controlling_process(Sock, Pid);
+controlling_process(Sock, Pid) ->
     ssl:controlling_process(Sock, Pid).
 
 peername(#tecipe_socket{inet_socket = Sock}) ->
+    ssl:peername(Sock);
+peername(Sock) ->
     ssl:peername(Sock).
 
 sockname(#tecipe_socket{inet_socket = Sock}) ->
+    ssl:sockname(Sock);
+sockname(Sock) ->
     ssl:sockname(Sock).
 
+getopts(#tecipe_socket{inet_socket = Sock}, Opts) ->
+    ssl:getopts(Sock, Opts);
+getopts(Sock, Opts) ->
+    ssl:getopts(Sock, Opts).
+
+setopts(#tecipe_socket{inet_socket = Sock}, Opts) ->
+    ssl:setopts(Sock, Opts);
+setopts(Sock, Opts) ->
+    ssl:setopts(Sock, Opts).
+
 getstat(#tecipe_socket{inet_socket = Sock}) ->
+    ssl:getstat(Sock);
+getstat(Sock) ->
     ssl:getstat(Sock).
 
 close(#tecipe_socket{inet_socket = Sock}) ->
+    ssl:close(Sock);
+close(Sock) ->
     ssl:close(Sock).
--- src/tecipe_tcp.erl
+++ src/tecipe_tcp.erl
@@ -1,7 +1,8 @@
 -module(tecipe_tcp).
 
--export([listen/2, accept/1, setopts/2, send/2, recv/2,
-	 controlling_process/2, peername/1, sockname/1, getstat/1, close/1]).
+-export([listen/2, accept/1, send/2, recv/2,
+	 controlling_process/2, peername/1, sockname/1,
+	 setopts/2, getopts/2, getstat/1, close/1]).
 
 -include("tecipe.hrl").
 
@@ -11,26 +12,47 @@ listen(Port, Opts) ->
 accept(ListeningSock) ->
     gen_tcp:accept(ListeningSock).
 
-setopts(#tecipe_socket{inet_socket = Sock}, Opts) ->
-    inet:setopts(Sock, Opts).
-
 send(#tecipe_socket{inet_socket = Sock}, Data) ->
+    gen_tcp:send(Sock, Data);
+send(Sock, Data) ->
     gen_tcp:send(Sock, Data).
 
 recv(#tecipe_socket{inet_socket = Sock}, Length) ->
+    gen_tcp:recv(Sock, Length);
+recv(Sock, Length) ->
     gen_tcp:recv(Sock, Length).
 
 controlling_process(#tecipe_socket{inet_socket = Sock}, Pid) ->
+    gen_tcp:controlling_process(Sock, Pid);
+controlling_process(Sock, Pid) ->
     gen_tcp:controlling_process(Sock, Pid).
 
 peername(#tecipe_socket{inet_socket = Sock}) ->
+    inet:peername(Sock);
+peername(Sock) ->
     inet:peername(Sock).
 
 sockname(#tecipe_socket{inet_socket = Sock}) ->
+    inet:sockname(Sock);
+sockname(Sock) ->
     inet:sockname(Sock).
 
+setopts(#tecipe_socket{inet_socket = Sock}, Opts) ->
+    inet:setopts(Sock, Opts);
+setopts(Sock, Opts) ->
+    inet:setopts(Sock, Opts).
+
+getopts(#tecipe_socket{inet_socket = Sock}, Opts) ->
+    inet:getopts(Sock, Opts);
+getopts(Sock, Opts) ->
+    inet:getopts(Sock, Opts).
+
 getstat(#tecipe_socket{inet_socket = Sock}) ->
+    inet:getstat(Sock);
+getstat(Sock) ->
     inet:getstat(Sock).
 
 close(#tecipe_socket{inet_socket = Sock}) ->
+    gen_tcp:close(Sock);
+close(Sock) ->
     gen_tcp:close(Sock).
